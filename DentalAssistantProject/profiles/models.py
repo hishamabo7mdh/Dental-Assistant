563ed2c28a44a6817fcb750c2edb3587f5dc2a4a
unknown <hishamabo7mdh@gmail.com>
2023-03-06 12:47:00 +0100

    v1

--- DentalAssistantProject/profiles/models.py
+++ DentalAssistantProject/profiles/models.py
@@ -9,13 +9,13 @@ class Profile(models.Model):
         ('female','female')
     ]
     name=models.CharField(max_length=100)
-    gender=models.CharField(max_length=10,null=True,blank=True,choices= genderCoices)
+    gender=models.CharField(max_length=10,choices= genderCoices)
     
     def __str__(self):
         return self.name
 
 class Visits(models.Model):
-    image=models.ImageField(upload_to='photos/%y/%m/%d')
-    description=models.TextField()
+    image=models.ImageField(null=True,blank=True,upload_to='photos/%y/%m/%d')
+    description=models.TextField(null=True,blank=True)
     dateCreated=models.DateTimeField(default=datetime.now)
-    profileID=models.ForeignKey(Profile , on_delete=models.CASCADE)        
+    profileID=models.ForeignKey(Profile , on_delete=models.PROTECT)        
